5f43a4a03df51dd104849026cce1e85026991f2e
snowcs66 <yuzhouzhou@zhouzhoufushideMacBook.local>
2021-11-23 23:39:09 -0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,12 @@
+public class HelloNumbers {
+	public static void main(String[] args) {
+		int x = 0;
+		int n = 0;
+		while (x < 10) {
+			n = n + x;
+			System.out.print(n + " ");
+			x = x + 1;
+		}
+		System.out.println(5 + "10");
+	}
+}
